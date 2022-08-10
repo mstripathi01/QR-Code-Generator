c5f8069e3f2d218f94bbaf173e46e4923ab2f481
MAYANK  TRIPATHI <mayank.tripathi@indianic.com>
2022-08-10 17:44:06 +0530

    qr code generator

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+
+generateBtn.addEventListener("click", () => {
+  let qrValue = qrInput.value;
+  if(!qrValue) return ; // empty return
+  generateBtn.innerText = "Generating QR Code...";
+  //generating api on user entered value using the qrserver
+  // api and passing the api returned img src to qrImg
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+  qrImg.addEventListener("load", () => { //once Qr code img loaded
+    wrapper.classList.add("active");
+    generateBtn.innerText = "Generating QR Code...";
+  });
+});
+
+qrInput.addEventListener("keyup", () => {
+  if(!qrInput.value){
+    wrapper.classList.remove("active");
+  }
+});
+
